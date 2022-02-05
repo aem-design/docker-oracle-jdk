aab71b6e0fd9a6d57f85611c70cac79fc0a239c2
wildone <max.barrass@gmail.com>
2022-02-06 02:03:55 +1100

    build amd64.

--- build.ps1
+++ build.ps1
@@ -5,7 +5,7 @@ Param(
   [string]$TAG = "jdk11",
   [string]$FILE = "Dockerfile",
   [string]$FUNCTIONS_URI = "https://github.com/aem-design/aemdesign-docker/releases/latest/download/functions.ps1",
-  [string]$COMMAND = "docker buildx build --platform linux/arm64 --build-arg JDK_DRIVEID=$env:JDK_DRIVEID . -f .\${FILE} -t ${TAG}"
+  [string]$COMMAND = "docker buildx build --build-arg JDK_DRIVEID=$env:JDK_DRIVEID . -f .\${FILE} -t ${TAG}"
 )
 
 $SKIP_CONFIG = $true
