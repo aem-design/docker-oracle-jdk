abdb30c3555f9f077b785f157747797d48856cdd
wildone <max.barrass@gmail.com>
2022-02-06 00:20:07 +1100

    update pipeline.

--- build.ps1
+++ build.ps1
@@ -2,10 +2,10 @@ Param(
   [string]$LOG_PATH = "${PWD}\logs",
   [string]$LOG_PEFIX = "docker",
   [string]$LOG_SUFFIX = ".log",
-  [string]$TAG = "jdk11",
+  [string]$TAG = "jdk8-arm",
   [string]$FILE = "Dockerfile",
   [string]$FUNCTIONS_URI = "https://github.com/aem-design/aemdesign-docker/releases/latest/download/functions.ps1",
-  [string]$COMMAND = "docker build --build-arg ORACLE_PASSWORD=$ORACLE_PASSWORD . -f .\${FILE} -t ${TAG}"
+  [string]$COMMAND = "docker buildx build --platform linux/arm64 --build-arg JDK_DRIVEID=$env:JDK_DRIVEID . -f .\${FILE} -t ${TAG}"
 )
 
 $SKIP_CONFIG = $true
@@ -14,7 +14,7 @@ $PARENT_PROJECT_PATH = "."
 . ([Scriptblock]::Create((([System.Text.Encoding]::ASCII).getString((Invoke-WebRequest -Uri "${FUNCTIONS_URI}").Content))))
 
 printSectionBanner "Building Image"
-printSectionLine ( $COMMAND -replace $ORACLE_PASSWORD, $( "*" * $ORACLE_PASSWORD.length ) ) "warn"
+printSectionLine ( $COMMAND -replace $JDK_DRIVEID, $( "*" * $JDK_DRIVEID.length ) ) "warn"
 
 Invoke-Expression -Command "$COMMAND" | Tee-Object -Append -FilePath "${LOG_FILE}"
 
