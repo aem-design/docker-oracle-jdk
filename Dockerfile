0865329ba3a1f2809920a2cb6ec56059546f8197
wildone <max.barrass@gmail.com>
2026-02-26 15:16:57 +1100

    Release jdk21: update build, scripts, and docs

--- Dockerfile
+++ Dockerfile
@@ -2,22 +2,24 @@ FROM        aemdesign/tini:ubuntu-focal
 
 LABEL   os="ubuntu focal" \
         container.description="oracle jdk" \
-        version="jdk17" \
+        version="jdk21" \
         maintainer="devops <devops@aem.design>" \
         imagename="oracle-jdk" \
         test.command=" java --version" \
-        test.command.verify="17."
+        test.command.verify="21."
 
-ARG FILE_NAME="jdk-17.0.14_linux-x64_bin.tar.gz"
+ARG FILE_NAME="jdk-21_linux-x64_bin.tar.gz"
 
-ENV JAVA_HOME="/opt/jdk-17.0.14/"
+ENV JAVA_HOME="/opt/jdk-21/"
 
 ADD packages/${FILE_NAME} /opt/
 
 RUN \
     echo "CONFIG JDK" && \
-    export JAVA_HOME=${JAVA_HOME}  && \
+    JDK_DIR="$(find /opt -maxdepth 1 -type d -name 'jdk-21*' | head -n 1)" && \
+    test -n "${JDK_DIR}" && \
+    ln -sfn "${JDK_DIR}" "${JAVA_HOME%/}" && \
     update-alternatives --install /usr/bin/java java ${JAVA_HOME%*/}/bin/java 1 && \
     update-alternatives --install /usr/bin/javac javac ${JAVA_HOME%*/}/bin/javac 1 && \
-    update-alternatives --config java && \
-    rm -rf /opt/${FILE_NAME}
+    update-alternatives --set java ${JAVA_HOME%*/}/bin/java && \
+    rm -f /opt/${FILE_NAME}
--- debug.ps1
+++ debug.ps1
@@ -2,8 +2,11 @@ Param(
   [string]$LOG_PATH = "${PWD}\logs",
   [string]$LOG_PEFIX = "docker",
   [string]$LOG_SUFFIX = ".log",
-  [string]$TAG = "jdk17",
+  [string]$TAG = "jdk21",
   [string]$FILE = "Dockerfile",
+  [string]$JDK_LOCAL_PATH = "C:\\data\\apps\\#dev\\jdk\\jdk-21.0.7",
+  [string]$JDK_PACKAGE_FILE = "jdk-21_linux-x64_bin.tar.gz",
+  [string]$JDK_URL = "https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.tar.gz",
   [string]$FUNCTIONS_URI = "https://github.com/aem-design/aemdesign-docker/releases/latest/download/functions.ps1",
   [string]$COMMAND = "docker build . -f .\${FILE} -t "
 )
@@ -16,7 +19,25 @@ $COMMAND="$COMMAND${IMAGENAME}:${IMAGEVERSION}"
 $SKIP_CONFIG = $true
 $PARENT_PROJECT_PATH = "."
 
-. ([Scriptblock]::Create((([System.Text.Encoding]::ASCII).getString((Invoke-WebRequest -Uri "${FUNCTIONS_URI}").Content))))
+try {
+  . ([Scriptblock]::Create((([System.Text.Encoding]::ASCII).getString((Invoke-WebRequest -Uri "${FUNCTIONS_URI}").Content))))
+} catch {
+  if (-not (Test-Path $LOG_PATH)) { New-Item -ItemType Directory -Path $LOG_PATH -Force | Out-Null }
+  $LOG_FILE = Join-Path $LOG_PATH ("{0}-{1}{2}" -f $LOG_PEFIX, (Get-Date -Format "yyyyMMdd-HHmmss"), $LOG_SUFFIX)
+  function printSectionBanner { param([string]$Text) Write-Host "==== $Text ====" }
+  function printSectionLine { param([string]$Text, [string]$Level) Write-Host $Text }
+}
+
+if (-not (Test-Path ".\\packages\\$JDK_PACKAGE_FILE")) {
+  New-Item -ItemType Directory -Path ".\\packages" -Force | Out-Null
+  if (Test-Path $JDK_LOCAL_PATH) {
+    Write-Host "Packaging local JDK from ${JDK_LOCAL_PATH}..."
+    tar -czf ".\\packages\\$JDK_PACKAGE_FILE" -C (Split-Path $JDK_LOCAL_PATH -Parent) (Split-Path $JDK_LOCAL_PATH -Leaf)
+  } else {
+    Write-Host "Downloading JDK from ${JDK_URL}..."
+    Invoke-WebRequest -Uri "${JDK_URL}" -OutFile ".\\packages\\$JDK_PACKAGE_FILE"
+  }
+}
 
 printSectionBanner "Loading Debug Image"
 printSectionLine "$COMMAND" "warn"
